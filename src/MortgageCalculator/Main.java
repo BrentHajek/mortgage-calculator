e7d20526237ea9b7aad6afb806cdc91ee3962458
BrentHajek <brenthajek@hotmail.com>
2021-11-15 21:01:50 -0500

    practice mortgage calculator program

---
+++ src/MortgageCalculator/Main.java
@@ -0,0 +1,55 @@
+package MortgageCalculator;
+
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        final byte monthsInYear = 12; //declaring these two as constants
+        final byte percent = 100;
+
+        int principal = 0;
+        float monthlyInterest = 0;
+        int numberOfPayments = 0;
+
+        Scanner input = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Principal ($1k - $1M): ");
+            principal = input.nextInt();
+            if (principal >= 1_000 && principal <= 1_000_000)
+                break;
+            System.out.println("Enter a number between $1,000 and $1,000,000.");
+        }
+
+        while (true) {
+            System.out.print("Annual Interest Rate: ");
+            float apr = input.nextFloat();
+            if (apr >= 1 && apr <= 30) {
+                monthlyInterest = apr / percent / monthsInYear;
+                break;
+            }
+            System.out.println("Enter a value greater than 0 and less than or equal to 30.");
+        }
+
+        while (true) {
+            System.out.print("Period (Years): ");
+            byte years = input.nextByte();
+            if (years >= 1 && years <= 30) {
+                numberOfPayments = years * monthsInYear;
+                break;
+            }
+            System.out.println("Enter a value between 1 and 30.");
+        }
+
+        double mortgage = principal
+                * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
+                /(Math.pow(1+ monthlyInterest, numberOfPayments) -1);
+
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.print("Mortgage: " + mortgageFormatted);
+
+    }
+}
